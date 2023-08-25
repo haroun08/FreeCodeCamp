b0afdddb56e8b54e50e706e0f22f7d1476866051
Haroun <129984358+haroun08@users.noreply.github.com>
2023-08-25 23:50:59 +0100

    Update cash-register.js

--- cash-register.js
+++ cash-register.js
@@ -45,5 +45,3 @@ function checkCashRegister(price, cash, cid) {
   }
   return { 'status': status, 'change': change };
 }
-
-console.log(checkCashRegister(27.75, 50, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));

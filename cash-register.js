7a50a8e3998b60addc267c896cc844728d39bfac
Haroun <129984358+haroun08@users.noreply.github.com>
2023-08-26 06:05:25 +0100

    Update cash-register.js

--- cash-register.js
+++ cash-register.js
@@ -1,47 +1,59 @@
 const currencyUnit = {
-  "PENNY": 1,
-  "NICKEL": 5,
-  "DIME": 10,
-  "QUARTER": 25,
-  "ONE": 100,
-  "FIVE": 500,
-  "TEN": 1000,
-  "TWENTY": 2000,
-  "ONE HUNDRED": 10000
+  "PENNY": 0.01,
+  "NICKEL": 0.05,
+  "DIME": 0.1,
+  "QUARTER": 0.25,
+  "ONE": 1,
+  "FIVE": 5,
+  "TEN": 10,
+  "TWENTY": 20,
+  "ONE HUNDRED": 100
 }
 
 function checkCashRegister(price, cash, cid) {
-  let changeSum = cash * 100 - price * 100;
-  let changeSumCheck = changeSum;
+  let changeDue = cash - price;
   let change = [];
   let status = '';
 
-  let cidSum = 0;
-  let filteredCid = cid.filter(elem => elem[1] !== 0).reverse();
+  let totalCid = cid.reduce((total, [, amount]) => total + amount, 0);
 
-  filteredCid.forEach(elem => {
-    let curr = elem[0];
-    let currSum = elem[1] * 100;
-    cidSum += currSum;
-    let amount = 0;
-    while (changeSum >= currencyUnit[curr] && currSum > 0) {
-      amount += currencyUnit[curr];
-      changeSum -= currencyUnit[curr];
-      currSum -= currencyUnit[curr];
-    }
-    if (amount !== 0) {
-      change.push([curr, amount / 100]);
-    }
-  });
-
-  if (changeSum > 0) {
+  if (totalCid < changeDue) {
     status = 'INSUFFICIENT_FUNDS';
-    change = [];
-  } else if (changeSum == 0 && changeSumCheck == cidSum) {
+  } else if (totalCid === changeDue) {
     status = 'CLOSED';
     change = cid;
   } else {
-    status = 'OPEN';
+    for (let i = cid.length - 1; i >= 0; i--) {
+      const [curr, available] = cid[i];
+      const unit = currencyUnit[curr];
+      
+      if (changeDue >= unit && available > 0) {
+        const count = Math.min(Math.floor(changeDue / unit), available / unit);
+        const amount = count * unit;
+        change.push([curr, amount]);
+        changeDue = parseFloat((changeDue - amount).toFixed(2));
+      }
+    }
+    
+    if (changeDue > 0) {
+      status = 'INSUFFICIENT_FUNDS';
+      change = [];
+    } else {
+      status = 'OPEN';
+    }
   }
+
   return { 'status': status, 'change': change };
 }
+
+console.log(checkCashRegister(27.75, 50, [
+  ["PENNY", 1.01], 
+  ["NICKEL", 2.05], 
+  ["DIME", 3.1], 
+  ["QUARTER", 4.25], 
+  ["ONE", 90], 
+  ["FIVE", 55], 
+  ["TEN", 20], 
+  ["TWENTY", 60], 
+  ["ONE HUNDRED", 100]
+]));
